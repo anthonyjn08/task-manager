b93c6f855ee937593aa087f2a065c82c62bb4729
Anthony Nelson <anthonyjnelson@hotmail.com>
2025-08-08 18:45:48 +0100

    Add and initialiase TaskService class in business_logic.py

--- business_logic.py
+++ business_logic.py
@@ -1,1 +1,7 @@
 from data_access import TaskRepository
+
+
+class TaskService:
+    def __init__(self):
+        """Initialise the TaskService with a TaskRepository"""
+        self.task_repository = TaskRepository()

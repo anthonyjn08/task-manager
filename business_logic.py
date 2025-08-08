02dbd586a1e918d0167af0ec8a867480baeda158
Anthony Nelson <anthonyjnelson@hotmail.com>
2025-08-08 18:53:42 +0100

    Add add_task() to business_logic.py

--- business_logic.py
+++ business_logic.py
@@ -5,3 +5,7 @@ class TaskService:
     def __init__(self):
         """Initialise the TaskService with a TaskRepository"""
         self.task_repository = TaskRepository()
+
+    def add_task(self, task):
+        "Create new task in database"
+        self.task_repository.add_task(task)

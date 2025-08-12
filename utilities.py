7c51cb3415aa9fffe402ac1a095f37b08cb2747a
Anthony Nelson <anthonyjnelson@hotmail.com>
2025-08-12 14:18:13 +0100

    Add date_validation() to utilities.py

---
+++ utilities.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+
+def date_validation(prompt):
+    '''
+    Function: date_validation
+    This function converts the string date when adding tasks into an
+     acutal date format that can be used when checking if a task
+     is overdue.
+
+    Input:
+    prompt: (str) asks users to input a date.
+
+    Output:
+    date_obj: (date) returns the user string in date format
+    '''
+    while True:
+        date_input = input(prompt)
+
+        try:
+            date_obj = datetime.strptime(date_input.strip(), "%d %b %Y")
+            return date_obj
+
+        except ValueError:
+            print("\nPlease enter a date in following format: 'DD Mon YYYY'.\n")
